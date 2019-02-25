a9732e9ddccbe5d050d77b110c48cb9d494ec5e7
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-02-24 20:49:40 -0600

    Adding a gradle project for practicing regular expressions

---
+++ RegularExpressionsWithJava/src/main/java/com/kingshuk/regularexpressions/basics/BasicRegularExpressionDemo.java
@@ -0,0 +1,27 @@
+package com.kingshuk.regularexpressions.basics;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BasicRegularExpressionDemo {
+
+	public static void main(String[] args) {
+		int count = 0;
+
+		// Defining the pattern
+		Pattern pattern = Pattern.compile("ab");
+
+		// Defining which string this pattern should be applied to
+		//The string passed to the matcher method is also known as the target string.
+		Matcher matcher = pattern.matcher("ahshdhssdjdhjabbbabababa");
+
+		while (matcher.find()) {
+			count++;
+
+			System.out.println(matcher.start() + "..." + matcher.end() + "...." + matcher.group());
+		}
+		
+		System.out.println("The number of occurences..."+count);
+	}
+
+}
