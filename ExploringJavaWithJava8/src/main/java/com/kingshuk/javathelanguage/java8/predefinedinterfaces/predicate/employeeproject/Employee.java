62350a5031eebc0ec54b8d4e5ec1b2e978f56177
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-03-17 22:25:58 -0500

    Added a predicate for checking dates as well

--- ExploringJavaWithJava8/src/main/java/com/kingshuk/javathelanguage/java8/predefinedinterfaces/predicate/employeeproject/Employee.java
+++ ExploringJavaWithJava8/src/main/java/com/kingshuk/javathelanguage/java8/predefinedinterfaces/predicate/employeeproject/Employee.java
@@ -1,15 +1,25 @@
 package com.kingshuk.javathelanguage.java8.predefinedinterfaces.predicate.employeeproject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.function.BiPredicate;
 import java.util.function.DoublePredicate;
+import java.util.function.Predicate;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+//@Slf4j
 public class Employee {
 
 	private String employeeName;
@@ -20,23 +30,13 @@ public class Employee {
 
 	private double salary;
 
-	private Employee(String employeeName, EmployeeLocation employeeLocation, Designation designation, double salary) {
-		super();
-		this.employeeName = employeeName;
-		this.employeeLocation = employeeLocation;
-		this.designation = designation;
-		this.salary = salary;
-	}
-
-	private Employee() {
-		super();
-	}
+	private Date joiningDate;
 
 	// Factory pattern using inner class
 	public static class FactoryMethodCreator {
 
 		public Employee createEmployee(String employeeName, EmployeeLocation employeeLocation, Designation designation,
-				double salary) {
+				double salary, String date) {
 
 			// Predicate<Double> checkSalary = s -> s >= designation.minimumSalary;
 
@@ -44,27 +44,44 @@ public class Employee {
 
 			BiPredicate<EmployeeLocation, Designation> checkDesignationAndLocation = (loc,
 					des) -> loc.equals(EmployeeLocation.BANGALORE) && des.equals(Designation.DEVELOPER);
-					
+
+			SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+			
+			Date joiningDate = null;
+			try {
+				joiningDate = format.parse(date);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+
+			Predicate<Date> datePredicate = datePassed -> datePassed.after(new Date());
+
 			String message = "";
 
-			if(checkSalary.negate().test(salary) ||
-					checkDesignationAndLocation.test(employeeLocation, designation)) {
+			if (checkSalary.negate().test(salary) || checkDesignationAndLocation.test(employeeLocation, designation)
+					|| datePredicate.test(joiningDate)) {
 				if (checkSalary.negate().test(salary)) {
-					message = String.format("Employee Salary must be greater than designation minimum of %.2f",designation.minimumSalary);
+					message = String.format("Employee Salary must be greater than designation minimum of %.2f",
+							designation.minimumSalary);
+				}
+
+				if (checkDesignationAndLocation.test(employeeLocation, designation)) {
+					message = message.concat(String.format(" A %s can't work from %s", designation.designationName,
+							employeeLocation.city));
+
 				}
 				
-				if(checkDesignationAndLocation.test(employeeLocation, designation)) {
-					message = message.concat(String.format(" A %s can't work from %s", designation.designationName, employeeLocation.city));
-					
-					
+				if(datePredicate.test(joiningDate)) {
+					message = message.concat("Joining date can't be before current date");
 				}
-				
+
 				throw new EmployeeCreationException(message);
 			}
 
-			return new Employee(employeeName, employeeLocation, designation, salary);
+			return new Employee(employeeName, employeeLocation, designation, salary, joiningDate);
 
 		}
+		
 	}
 
 	public static FactoryMethodCreator createInnerClass() {
--- ExploringJavaWithJava8/src/main/java/com/kingshuk/javathelanguage/java8/predefinedinterfaces/predicate/employeeproject/PredicateEmployeeProjectHarness.java
+++ ExploringJavaWithJava8/src/main/java/com/kingshuk/javathelanguage/java8/predefinedinterfaces/predicate/employeeproject/PredicateEmployeeProjectHarness.java
@@ -9,17 +9,17 @@ public class PredicateEmployeeProjectHarness {
 
 	public static void main(String[] args) {
 		List<Employee> employeeList = null;
-
+		
 		Employee employee1 = Employee.createInnerClass().createEmployee("Kingshuk Mukherjee", EmployeeLocation.CHENNAI,
-				Designation.DEVELOPER, 68000.00);
+				Designation.DEVELOPER, 68000.00, "03/29/2010");
 		Employee employee2 = Employee.createInnerClass().createEmployee("Deeksha Banerjee", EmployeeLocation.CHICAGO,
-				Designation.MANAGER, 110000.00);
+				Designation.MANAGER, 110000.00,"04/28/2010");
 		Employee employee3 = Employee.createInnerClass().createEmployee("Amit Biswas", EmployeeLocation.CHENNAI,
-				Designation.MANAGER, 120000.00);
+				Designation.MANAGER, 120000.00,"03/19/2010");
 		Employee employee4 = Employee.createInnerClass().createEmployee("Mayuri Srivastava", EmployeeLocation.KOLKATA,
-				Designation.DEVELOPER, 69000.00);
+				Designation.DEVELOPER, 69000.00,"03/29/2012");
 		Employee employee5 = Employee.createInnerClass().createEmployee("Mainak Biswas", EmployeeLocation.BANGALORE,
-				Designation.TESTER, 65000.00);
+				Designation.TESTER, 65000.00,"03/29/2018");
 
 		employeeList = Arrays.asList(employee1, employee2, employee3, employee4, employee5);
 
