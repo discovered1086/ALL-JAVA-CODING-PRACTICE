99d6e39f6d16d14aff67aba98ad4233675cd9978
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-03-17 21:47:22 -0500

    Adding more predicates for practice

--- ExploringJavaWithJava8/src/main/java/com/kingshuk/javathelanguage/java8/predefinedinterfaces/predicate/employeeproject/PredicateEmployeeProjectHarness.java
+++ ExploringJavaWithJava8/src/main/java/com/kingshuk/javathelanguage/java8/predefinedinterfaces/predicate/employeeproject/PredicateEmployeeProjectHarness.java
@@ -1,6 +1,5 @@
 package com.kingshuk.javathelanguage.java8.predefinedinterfaces.predicate.employeeproject;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
@@ -21,27 +20,43 @@ public class PredicateEmployeeProjectHarness {
 				Designation.DEVELOPER, 69000.00);
 		Employee employee5 = Employee.createInnerClass().createEmployee("Mainak Biswas", EmployeeLocation.BANGALORE,
 				Designation.TESTER, 65000.00);
-		
+
 		employeeList = Arrays.asList(employee1, employee2, employee3, employee4, employee5);
 
-		//System.out.println("The employee list is:" + employeeList);
-		
+		// System.out.println("The employee list is:" + employeeList);
+
 		Predicate<Employee> selectManagers = employee -> employee.getDesignation().equals(Designation.MANAGER);
 		
-		
-		//Printing managers list without streams
-		/*employeeList.forEach((emp) -> {
-			if(selectManagers.test(emp)) {
-				System.out.println("The managers in this project are: "+emp.getEmployeeName());
-			}
-		});*/
-		
-		//Printing managers list with streams
-		List<Employee> managersList = employeeList.stream().filter(selectManagers).collect(Collectors.toList());
-		
-		managersList.forEach(emp -> System.out.println("The managers in this project are: "+emp.getEmployeeName()));
-		
-		
+		Predicate<Employee> selectDevelopers = employee -> employee.getDesignation().equals(Designation.DEVELOPER);
+
+		System.out.println("-------------Filtering managers from the list--------------");
+
+		filterEmployees(selectManagers, employeeList);
+
+		System.out.println("-------------Filtering managers from the list--------------");
+
+		System.out.println("-------------Filtering developers from the list--------------");
+
+		filterEmployees(selectDevelopers, employeeList);
+
+		System.out.println("-------------Filtering developers from the list--------------");
+	}
+
+	public static void filterEmployees(Predicate<Employee> thePredicate, List<Employee> employeeList) {
+
+		// Printing managers list without streams
+		/*
+		 * employeeList.forEach((emp) -> { if(selectManagers.test(emp)) {
+		 * System.out.println("The managers in this project are: "+emp.getEmployeeName()
+		 * ); } });
+		 */
+
+		// Printing managers list with streams
+		List<Employee> managersList = employeeList.stream().filter(thePredicate).collect(Collectors.toList());
+
+		System.out.println("The selected employees are: ");
+
+		managersList.forEach(emp -> System.out.println(emp.getEmployeeName()));
 	}
 
 }
