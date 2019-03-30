e5e74886a816725cbc6180aa4b0c13e85fa95c4d
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-03-29 19:41:25 -0500

    Multi threading gradle project

--- MultiThreading/src/com/kingshuk/multithreading/threadprogramming/threadingin2017/synchronization/bankcaccountimproved/ActionTypes.java
+++
@@ -1,8 +0,0 @@
-package com.kingshuk.multithreading.threadprogramming.threadingin2017.synchronization.bankcaccountimproved;
-
-public enum ActionTypes {
-    DEBIT,
-    CREDIT,
-    CHECK_BALANCE,
-    UPDATE_PERSONAL_INFO
-}
