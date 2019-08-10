f4be1518df21d1e197a644060ab326548c9735a7
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-08-10 18:59:13 +0530

    Simple demonstration of date and formatting

---
+++ JavaDateAndTime/src/main/java/com/kingshuk/corejavaprojects/javadateandtime/afterjava8/BasicDateTimePractice.java
@@ -0,0 +1,38 @@
+package com.kingshuk.corejavaprojects.javadateandtime.afterjava8;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.Year;
+
+public class BasicDateTimePractice {
+
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		
+		System.out.println(date);
+		
+		LocalTime localTime = LocalTime.now();
+		
+		System.out.println(localTime);
+		
+		int day = date.getDayOfMonth();
+		
+		System.out.println(day);
+		
+		int month = date.getMonthValue();
+		
+		System.out.println(month);
+		
+		Month month2 = date.getMonth();
+		
+		System.out.println(month2);
+		
+		int year = date.getYear();
+		
+		System.out.println(year);
+		
+		System.out.printf("%d/%d/%d",day, month, year);
+	}
+
+}
