c8b55df3720825ea815bffad6975c0f0b123cf58
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-03-12 21:04:02 -0500

    Beginning with Duration

---
+++ JavaDateAndTime/src/main/java/com/kingshuk/corejavaprojects/javadateandtime/afterjava8/DurationAndInstantTest.java
@@ -0,0 +1,22 @@
+package com.kingshuk.corejavaprojects.javadateandtime.afterjava8;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class DurationAndInstantTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		Instant now1 = Instant.now();
+		System.out.println(now1);
+
+		Thread.sleep(5000);
+
+		Instant now2 = Instant.now();
+
+		Duration duration = Duration.between(now1, now2);
+
+		System.out.println(duration.toMillis());
+
+	}
+
+}
