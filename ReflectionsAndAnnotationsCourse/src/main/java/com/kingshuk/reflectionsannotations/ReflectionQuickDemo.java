55b29fc1316dff98c62f35face20270a76f0b9b3
Kingshuk Mukherjee <sly.mania42@gmail.com>
2020-05-26 20:20:09 -0500

    First commit for this branch

---
+++ ReflectionsAndAnnotationsCourse/src/main/java/com/kingshuk/reflectionsannotations/ReflectionQuickDemo.java
@@ -0,0 +1,24 @@
+package com.kingshuk.reflectionsannotations;
+
+import java.lang.reflect.Constructor;
+
+class PrivateConstructorClass {
+
+	private PrivateConstructorClass() {
+		System.out.println("Class object created");
+	}
+}
+
+public class ReflectionQuickDemo {
+
+	public static void main(String[] args) throws Exception{
+		// PrivateConstructorClass className = new PrivateConstructorClass();
+		Class<?> class1 = Class.forName("com.kingshuk.reflectionsannotations.PrivateConstructorClass");
+		Constructor<?> constructor = class1.getDeclaredConstructor();
+
+		constructor.setAccessible(true);
+
+		constructor.newInstance();
+	}
+
+}
